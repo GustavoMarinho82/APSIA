61f962cc9f177845e1e65e78be3859c04ce1dece
Henrique <henriquenunesrj09@gmail.com>
2023-10-14 11:48:32 -0300

    Conversão do Apsia

---
+++ FrontEnd/javascript/pedido.js
@@ -0,0 +1,48 @@
+function showPopup(text) {
+  var popupOverlay = document.getElementById('popupOverlay');
+  var popupText = document.getElementById('popupText');
+
+  popupOverlay.style.display = 'flex';
+}
+
+
+function hidePopup() {
+  var popupOverlay = document.getElementById('popupOverlay');
+  popupOverlay.style.display = 'none';
+}
+
+//funções CRUD
+
+function criar(){
+  var tipo = document.getElementById("tipo").innerHTML;
+  var orcamento = document.getElementById("orca").innerHTML
+  var data = document.getElementById("data").innerHTML
+  var numconvidado = document.getElementById("numconvidado").innerHTML
+  var endereco = document.getElementById("endereco").innerHTML
+  var observacao = document.getElementById("obs").innerHTML
+
+  var e = criarPedido( tipo, orcamento, data, data, numconvidado, endereco, observacao, 0, 0, 0, 0);
+
+  e.then( o => {
+    if ( o.data == "Pedido cadastrado com sucesso!"){
+        listarPedido();
+        notifications(o.data)
+    } else {
+        notifications(o.data, "Message--orange", "yes")
+        console.log(o.data)
+    }
+})
+}
+
+function del(){}
+function alterar(){}
+function listar(){}
+
+
+idPedido = 0;
+function redirecionar(){
+  window.location.href = "../views/comidas_cliente.html?idPedido=" + idPedido;
+}
+
+
+
