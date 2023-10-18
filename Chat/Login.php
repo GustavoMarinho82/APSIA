650a0731c005a9a9f1b7ff688b434c96b80649d0
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2023-10-18 17:43:41 -0300

    Javascripts funcionando no servidor

--- Chat/Login.php
+++ Chat/Login.php
@@ -48,7 +48,20 @@
             </section>
         </div>
 
-        <script src="javascript/pass-show-hide.js"></script>
+        <script>
+            const pswrdField = document.querySelector(".form input[type='password']"),
+            toggleIcon = document.querySelector(".form .field i");
+
+            toggleIcon.onclick = () =>{
+            if(pswrdField.type === "password"){
+                pswrdField.type = "text";
+                toggleIcon.classList.add("active");
+            }else{
+                pswrdField.type = "password";
+                toggleIcon.classList.remove("active");
+            }
+            }
+        </script>
 
     </body>
 </html>
