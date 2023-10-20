a631739ac495bd63fbf9ad73ae2b7170f580f265
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2023-10-20 20:57:57 -0300

    ponto

--- Admin/index.php
+++ Admin/index.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="" />
+<link rel="icon" type="image/x-icon" href="../imagens/icone.png" />
     <title>APSIA</title>
   </head>
   
--- Chat/header.php
+++ Chat/header.php
@@ -7,7 +7,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" type="image/x-icon" href="../imagens/icone.png" />
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+        
         <title>APSIA</title>
     </head>
